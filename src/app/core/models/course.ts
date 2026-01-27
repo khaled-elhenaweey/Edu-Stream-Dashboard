942cb9a2e7049b9487ba6d6e0ffc124edc0296d5
khaled-elhenaweey <khaledelhenaweey@gmail.com>
2026-01-27 18:10:13 +0200

    set the course interface

---
+++ src/app/core/models/course.ts
@@ -0,0 +1,14 @@
+export type CourseLevel = 'Beginner' | 'Intermediate' | 'Expert';
+export type CourseStatus = 'Active' | 'Completed' | 'Archived';
+export interface Course {
+  id:string;
+  title: string;
+  instructor:string;
+  price:number;
+  duration:string;
+  level:CourseLevel;
+  status:CourseStatus;
+  progress:number;
+  imageUrl:string;
+  rating:number;
+}
