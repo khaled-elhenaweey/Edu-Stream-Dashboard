b512bcb4672ed983f07c519090e455ad2fcdaf49
khaled-elhenaweey <khaledelhenaweey@gmail.com>
2026-01-27 19:13:25 +0200

    set the main styling in the app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Header } from './layout/header/header';
+import { Dashboard } from './features/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Header, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
