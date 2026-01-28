5dc8c6f64ca53e3ab3c4ffebe06722c35d3354f5
khaled-elhenaweey <khaledelhenaweey@gmail.com>
2026-01-28 12:01:50 +0200

    set the dashboard and card components logic and styling

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Course } from './../../core/models/course';
+import { Course as CourseService } from './../../core/services/course';
+import { Component, OnInit } from '@angular/core';
+import { CourseCard } from '../../shared/components/course-card/course-card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CourseCard, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+  coursesList: Course[] = [];
+  constructor(private courseService: CourseService) {}
 
+  ngOnInit(): void {
+    this.coursesList = this.courseService.getCourses();
+  }
 }
--- src/app/shared/components/course-card/course-card.ts
+++ src/app/shared/components/course-card/course-card.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Course } from '../../../core/models/course';
 
 @Component({
   selector: 'app-course-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-card.html',
   styleUrl: './course-card.css',
 })
 export class CourseCard {
-
+  @Input({ required: true }) course!: Course;
 }
