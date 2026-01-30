0ebe2aecad491254335760af002e9c28568f6db0
khaled-elhenaweey <khaledelhenaweey@gmail.com>
2026-01-30 14:31:04 +0200

    finish the modal logic

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,31 +1,40 @@
 import { Course } from './../../core/models/course';
 import { Course as CourseService } from './../../core/services/course';
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CourseCard } from '../../shared/components/course-card/course-card';
 import { CommonModule } from '@angular/common';
+import { Modal } from '../../shared/components/modal/modal';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CourseCard, CommonModule],
+  imports: [CourseCard, Modal, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard implements OnInit, OnChanges {
+export class Dashboard implements OnInit {
+  selectedCourseId: string | null = null;
+  isModalOpen: boolean = false;
   coursesList: Course[] = [];
   constructor(private courseService: CourseService) {}
-  ngOnChanges(changes: SimpleChanges): void {
-    this.resetCourses();
-  }
 
   ngOnInit(): void {
     this.coursesList = this.courseService.getCourses();
   }
 
   handleDelete(id: string) {
-    this.courseService.deleteCourse(id);
-    this.coursesList = this.courseService.getCourses();
+    this.selectedCourseId = id;
+    this.isModalOpen = true;
   }
-  resetCourses() {
-    this.coursesList = this.courseService.getCourses();
+
+  confirmDeletion() {
+    if (this.selectedCourseId) {
+      this.courseService.deleteCourse(this.selectedCourseId);
+      this.coursesList = this.courseService.getCourses();
+      this.closeModal();
+    }
+  }
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedCourseId = null;
   }
 }
--- src/app/shared/components/modal/modal.ts
+++ src/app/shared/components/modal/modal.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './modal.css',
 })
 export class Modal {
+  @Output() onClose = new EventEmitter<void>();
 
+  close() {
+    this.onClose.emit();
+  }
 }
