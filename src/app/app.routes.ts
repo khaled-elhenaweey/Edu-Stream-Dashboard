1aa2d9ec9ba68eb4e6ba3e0f2886cb0e3d89975b
khaled-elhenaweey <khaledelhenaweey@gmail.com>
2026-02-21 16:25:58 +0200

    add add course routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './features/dashboard/dashboard';
 import { CourseDetails } from './shared/components/course-details/course-details';
+import { AddCourse } from './features/add-course/add-course';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: Dashboard },
   { path: 'course/:id', component: CourseDetails },
+  { path: 'add-course', component: AddCourse },
 ];
